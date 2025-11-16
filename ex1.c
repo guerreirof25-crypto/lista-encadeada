d05054572dae8b4ed25c479f5df1644a2ff62e1a
guerreirof25-crypto <guerreirof25@gmail.com>
2025-11-16 11:30:22 -0300

    Correct formatting and syntax in ex1.c
    Fix formatting and syntax issues in ex1.c.

--- ex1.c
+++ ex1.c
@@ -7,25 +7,20 @@ struct Caixa {
 
 int main() {
     struct Caixa c1, c2, c3, c4, c5;
-
     c1.valor = 1;
     c2.valor = 3;
     c3.valor = 9;
     c4.valor = 7;
     c5.valor = 5;
-
     c1.prox = &c2;
     c2.prox = &c3;
     c3.prox = &c4;
     c4.prox = &c5;
     c5.prox = NULL;
-
     struct Caixa* p = &c1;
-
     while (p != NULL) {
         printf("%d ", p->valor);
         p = p->prox;
     }
-
     return 0;
 }

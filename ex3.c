aab5c8ff305eb6e02a989a982a4af2e3dfc91571
guerreirof25-crypto <guerreirof25@gmail.com>
2025-11-16 11:29:19 -0300

    Add linked list structure and display logic
    Implement a linked list structure and display function.

---
+++ ex3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+struct Caixa {
+    int valor;
+    struct Caixa* prox;
+};
+
+void exibe(struct Caixa* caixa) {
+    while (caixa != NULL) {
+        printf("%d ", caixa->valor);
+        caixa = caixa->prox;
+    }
+}
+int main() {
+    struct Caixa c1, c2, c3, c4, c5;
+    c1.valor = 1;
+    c2.valor = 3;
+    c3.valor = 9;
+    c4.valor = 7;
+    c5.valor = 5;
+    c1.prox = &c2;
+    c2.prox = &c3;
+    c3.prox = &c4;
+    c4.prox = &c5;
+    c5.prox = NULL;
+    exibe(&c1);
+    struct Caixa* cabeca = &c1;
+    struct Caixa* removida = c1.prox->prox;
+    c1.prox->prox = removida->prox;
+    exibe(cabeca);
+    removida->prox = cabeca;
+    cabeca = removida;
+    exibe(cabeca);
+    return 0;
+}

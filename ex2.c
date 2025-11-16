a2478ed8df933fe9b2839ee7473c3a90b8270f21
guerreirof25-crypto <guerreirof25@gmail.com>
2025-11-16 11:29:46 -0300

    Remove unnecessary whitespace in ex2.c

--- ex2.c
+++ ex2.c
@@ -14,20 +14,16 @@ void exibe(struct Caixa* caixa) {
 
 int main() {
     struct Caixa c1, c2, c3, c4, c5;
-
     c1.valor = 1;
     c2.valor = 3;
     c3.valor = 9;
     c4.valor = 7;
     c5.valor = 5;
-
     c1.prox = &c2;
     c2.prox = &c3;
     c3.prox = &c4;
     c4.prox = &c5;
     c5.prox = NULL;
-
     exibe(&c1);
-
     return 0;
 }

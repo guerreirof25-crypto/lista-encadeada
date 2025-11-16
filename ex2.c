aace18ecb6947dffe8ef7e426b27904234c82d64
guerreirof25-crypto <guerreirof25@gmail.com>
2025-11-16 11:26:43 -0300

    Add linked list implementation in ex2.c
    Implement a linked list structure and display function.

---
+++ ex2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct Caixa {
+    int valor;
+    struct Caixa* prox;
+};
+
+void exibe(struct Caixa* caixa) {
+    while (caixa != NULL) {
+        printf("%d ", caixa->valor);
+        caixa = caixa->prox;
+    }
+}
+
+int main() {
+    struct Caixa c1, c2, c3, c4, c5;
+
+    c1.valor = 1;
+    c2.valor = 3;
+    c3.valor = 9;
+    c4.valor = 7;
+    c5.valor = 5;
+
+    c1.prox = &c2;
+    c2.prox = &c3;
+    c3.prox = &c4;
+    c4.prox = &c5;
+    c5.prox = NULL;
+
+    exibe(&c1);
+
+    return 0;
+}
